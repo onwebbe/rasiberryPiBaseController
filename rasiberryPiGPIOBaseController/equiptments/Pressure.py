16ddd2f745713cac53aa0c4e56544b23d6dfc9ee
Tai WU <tai.wu@sap.com>
2020-04-08 14:53:16 +0800

    add presure

--- rasiberryPiGPIOBaseController/equiptments/Pressure.py
+++ rasiberryPiGPIOBaseController/equiptments/Pressure.py
@@ -24,23 +24,23 @@ while True:
 
   temp = bmp.read_temperature()
 
-# Read the current barometric pressure level
+  # Read the current barometric pressure level
 
   pressure = bmp.read_pressure()
 
-# To calculate altitude based on an estimated mean sea level pressure
+  # To calculate altitude based on an estimated mean sea level pressure
 
-# (1013.25 hPa) call the function as follows, but this won't be very accurate
+  # (1013.25 hPa) call the function as follows, but this won't be very accurate
 
   altitude = bmp.read_altitude()
 
-# To specify a more accurate altitude, enter the correct mean sea level
+  # To specify a more accurate altitude, enter the correct mean sea level
 
-# pressure level. For example, if the current pressure level is 1023.50 hPa
+  # pressure level. For example, if the current pressure level is 1023.50 hPa
 
-# enter 102350 since we include two decimal places in the integer value
+  # enter 102350 since we include two decimal places in the integer value
 
-# altitude = bmp.readAltitude(102350)
+  # altitude = bmp.readAltitude(102350)
 
   print ("Temperature: %.2f C" % temp)
 
