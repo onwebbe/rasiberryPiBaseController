65bcad893f84a745d29163b92aebca8f88e6b25d
Tai WU <tai.wu@sap.com>
2020-04-14 21:06:29 +0800

    fix mock

--- rasiberryPiGPIOBaseController/RasiberryPiGPIO.py
+++ rasiberryPiGPIOBaseController/RasiberryPiGPIO.py
@@ -70,7 +70,7 @@ class RasiberryPiGPIO:
 
   def _getPinByPITypeAuto(self):
     gpioStatusString = None
-    if (hasattr(GPIO, 'MOCK') and GPIO.MOCK == True):
+    if (hasattr(GPIO, 'MOCK')):
       gpioStatusString = sampleGPIOStr
     else:
       output = os.popen('gpio readall')
