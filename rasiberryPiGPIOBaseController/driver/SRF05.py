44da47b052a2ab36ff01911278ea583b24975f3e
Tai WU <tai.wu@sap.com>
2020-06-21 22:11:22 +0800

    fix sonar distance device issues

--- rasiberryPiGPIOBaseController/driver/SRF05.py
+++ rasiberryPiGPIOBaseController/driver/SRF05.py
@@ -8,25 +8,32 @@ class SRF05:
   def __init__(self, trigPinObj, echoPinObj):
     self._trigPinObj = trigPinObj
     self._echoPinObj = echoPinObj
-    self._trigPinObj.setupOutput()
-    self._echoPinObj.setupInput()
+    GPIO.setup(5, GPIO.OUT)
+    GPIO.setup(6, GPIO.IN)
     self._startTime = 0
     self._endTime = 0
     self._startTriger = 0
   
-  def _getEchoHIGH(self):
-    if (self._startTriger == 1):
-      self._endTime = time.time()
-      self._startTriger = 0
-      print(str(self._calculateDistance()))
-  
   def _calculateDistance(self):
     distanceTime = self._endTime - self._startTime
-    distance = 34000 * distanceTime
+    distance = 34000 * distanceTime / 2
     return distance
+
+  def _startToTriger(self):
+    GPIO.output(5, GPIO.HIGH)
+    # self._startTime = time.time()
+    time.sleep(0.01)
+    GPIO.output(5, GPIO.LOW)
 
   def startCheckDistance(self):
     self._checkEchoProcessor = None
-    self._echoPinObj.addChangeListener(Pin.PIN_PULL_UP, self._getEchoHIGH)
     self._startTriger = 1
+    _thread.start_new_thread(self._startToTriger, ())
+    while(not GPIO.input(6) == GPIO.HIGH):
+      pass
     self._startTime = time.time()
+    
+    while(not GPIO.input(6) == GPIO.LOW):
+      pass
+    self._endTime = time.time()
+    print(self._calculateDistance())
