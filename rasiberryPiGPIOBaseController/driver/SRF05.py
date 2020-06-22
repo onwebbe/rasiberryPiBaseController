d9be7b4cd91175e1c0e18bccbf5b4bb6d5e03118
Tai WU <tai.wu@sap.com>
2020-06-22 15:54:08 +0800

    remove hardcoded

--- rasiberryPiGPIOBaseController/driver/SRF05.py
+++ rasiberryPiGPIOBaseController/driver/SRF05.py
@@ -8,8 +8,8 @@ class SRF05:
   def __init__(self, trigPinObj, echoPinObj):
     self._trigPinObj = trigPinObj
     self._echoPinObj = echoPinObj
-    GPIO.setup(5, GPIO.OUT)
-    GPIO.setup(6, GPIO.IN)
+    self._trigPinObj.setupOutput()
+    self._echoPinObj.setupInput()
     self._startTime = 0
     self._endTime = 0
     self._startTriger = 0
@@ -20,20 +20,20 @@ class SRF05:
     return distance
 
   def _startToTriger(self):
-    GPIO.output(5, GPIO.HIGH)
+    self._trigPinObj.output_setup(Pin.PIN_HIGH)
     # self._startTime = time.time()
     time.sleep(0.01)
-    GPIO.output(5, GPIO.LOW)
+    self._trigPinObj.output_setup(Pin.PIN_LOW)
 
   def startCheckDistance(self):
     self._checkEchoProcessor = None
     self._startTriger = 1
     _thread.start_new_thread(self._startToTriger, ())
-    while(not GPIO.input(6) == GPIO.HIGH):
+    while(not self._echoPinObj.read() == GPIO.HIGH):
       pass
     self._startTime = time.time()
     
-    while(not GPIO.input(6) == GPIO.LOW):
+    while(not self._echoPinObj.read() == GPIO.LOW):
       pass
     self._endTime = time.time()
     return self._calculateDistance()
