62f06623f47dd633d10c6c73e4ea870c4b8bea59
Tai WU <tai.wu@sap.com>
2021-03-28 00:53:14 +0800

    add lcd weather display

---
+++ rasiberryPiGPIOBaseController/equiptments/LCDDisplay.py
@@ -0,0 +1,80 @@
+import rasiberryPiGPIOBaseController.driver.LCD1602 as LCD1602
+import sys
+import rasiberryPiGPIOBaseController.RasiberryPiGPIO as RasiberryPiGPIO
+import rasiberryPiGPIOBaseController.Pin as Pin
+import rasiberryPiGPIOBaseController.driver.BMP180 as BMP180
+import rasiberryPiGPIOBaseController.equiptments.LightSensor as LightSensor
+import rasiberryPiGPIOBaseController.equiptments.Temperature as Temperature
+import rasiberryPiGPIOBaseController.equiptments.SimpleEquipt as SimpleEquipt
+import time
+
+class LCD1602WeatherDisplay:
+  def __init__(self, rsPin, enablePin, D4Pin, D5Pin, D6Pin, D7Pin):
+    self._device = LCD1602.LCD1602(rsPin, enablePin, D4Pin, D5Pin, D6Pin, D7Pin)
+    newCharacter1 = [0x04, 0x06, 0x04, 0x06, 0x04, 0x04, 0x0e, 0x0e]
+    newCharacter2 = [0x04, 0x08, 0x0a, 0x12, 0x11, 0x11, 0x0a, 0x04]
+    allCharacters = {
+      "temperature": newCharacter1,
+      "humidity": newCharacter2
+    }
+    self._device.createNewCharacterInOnce(allCharacters)
+    self.displayCharForLine1([self._device.getNewCharacter('temperature')], self._device.convertToHEXForChar(" 0C    "),
+                              [self._device.getNewCharacter('humidity')], self._device.convertToHEXForChar(" 0%    "))
+    self.displayCharForLine2([self._device.getNewCharacter('temperature')], self._device.convertToHEXForChar(" 0l    "),
+                              [self._device.getNewCharacter('humidity')], self._device.convertToHEXForChar(" 0rpm  "))
+  
+  def displayWeather(self, temperature, humidity, light, wind):
+    self.displayCharFromPositionForLine1(3, self._device.convertToHEXForChar(str(temperature) + 'C'))
+    self.displayCharFromPositionForLine1(11, self._device.convertToHEXForChar(str(humidity) + '%'))
+    self.displayCharFromPositionForLine2(3, self._device.convertToHEXForChar(str(light) + 'l'))
+    self.displayCharFromPositionForLine2(11, self._device.convertToHEXForChar(str(wind) + 'rpm'))
+
+  def displayCharForLine1(self, *args):
+    self._device.displayChar(self, LCD1602.LCD_LINE_1, args)
+
+  def displayCharForLine2(self, *args):
+    self._device.displayChar(self, LCD1602.LCD_LINE_2, args)
+  
+  def displayCharFromPositionForLine1(self, position, *args):
+    self._device.displayCharFromPosition(LCD1602.LCD_LINE_1, position, args)
+  
+  def displayCharFromPositionForLine2(self, position, *args):
+    self._device.displayCharFromPosition(LCD1602.LCD_LINE_2, position, args)
+
+# isInit = False
+# if len(sys.argv) == 1:
+#   if sys.argv[0] == "true":
+#     isInit = True
+
+pi = RasiberryPiGPIO.RasiberryPiGPIO('3B+', 'BCM')
+rsPin = pi.getPin(23)
+ePin = pi.getPin(24)
+
+D4Pin = pi.getPin(25)
+D5Pin = pi.getPin(1)
+D6Pin = pi.getPin(12)
+D7Pin = pi.getPin(16)
+
+bmp = BMP180.BMP180()
+
+
+gy = LightSensor.GY30()
+
+
+dht22Pin = pi.getPin(27)
+dht = Temperature.DHT22(dht22Pin)
+
+windPin = pi.getPin(17)
+
+wind = SimpleEquipt.HSensorRotationV2.getInstance(windPin)
+weatherDisplay = LCD1602WeatherDisplay(rsPin, ePin, D4Pin, D5Pin, D6Pin, D7Pin)
+
+while True:
+  pressure = bmp.read_pressure()
+  light = gy.getLightData()
+  dhtData = dht.getData()
+  temperature = dhtData[0]
+  humidity = dhtData[1]
+  windSpeed = wind.getAvgData(5)
+  weatherDisplay.displayWeather(temperature, humidity, light, windSpeed)
+  time.sleep(10)
