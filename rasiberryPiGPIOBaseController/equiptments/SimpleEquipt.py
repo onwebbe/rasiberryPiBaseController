a734a65af3e1fb3282220b2ed7db1afda19a3bf8
Tai WU <tai.wu@sap.com>
2021-04-03 20:12:47 +0800

    fix issue

--- rasiberryPiGPIOBaseController/equiptments/SimpleEquipt.py
+++ rasiberryPiGPIOBaseController/equiptments/SimpleEquipt.py
@@ -204,7 +204,7 @@ class HSensorRotationV2:
   
   def getData(self, num):
     size = len(self._sensorDataList)
-    if ( size >= (num - 1) ):
+    if ( size > (num - 1) ):
       afterTime = self._sensorDataList[size - num]
       beforeTime = self._sensorDataList[size - num - 1]
       # when there are no rotate time will be 0, so this data is skipped
